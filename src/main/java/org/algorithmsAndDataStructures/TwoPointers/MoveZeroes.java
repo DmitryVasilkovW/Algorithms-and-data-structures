cbd58b59b71d01ce894b6abde72e8f8cdef16d9f
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-04-27 00:27:15 +0300

    feat: prefix sum and two pointers

--- src/main/java/org/algorithmsAndDataStructures/TwoPointers/MoveZeroes.java
+++ src/main/java/org/algorithmsAndDataStructures/TwoPointers/MoveZeroes.java
@@ -1,2 +1,46 @@
-package org.algorithmsAndDataStructures.TwoPointers;public class MoveZeroes {
+package org.algorithmsAndDataStructures.TwoPointers;
+// https://leetcode.com/problems/move-zeroes/description/?envType=study-plan-v2&envId=leetcode-75
+
+
+public class MoveZeroes
+{
+    public static void moveZeroes(int[] nums)
+    {
+        int r = 0, l = -1;
+
+        for (int i = 0; i < nums.length; i++)
+        {
+
+            if (nums[i] == 0)
+            {
+                r = i + 1;
+
+                if (r > nums.length - 1)
+                {
+                    break;
+                }
+
+                if (l == -1)
+                {
+                    l = i;
+                }
+            }
+
+            if (nums[r] != 0 && l >= 0)
+            {
+                int tmp = nums[r];
+                nums[r] = nums[l];
+                nums[l] = tmp;
+
+                l++;
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int[] nums = {1,0,1};
+
+        moveZeroes(nums);
+    }
 }
--- src/main/java/org/algorithmsAndDataStructures/prefixSum/FindPivotIndex.java
+++ src/main/java/org/algorithmsAndDataStructures/prefixSum/FindPivotIndex.java
@@ -1,2 +1,83 @@
-package org.algorithmsAndDataStructures.prefixSum;public class FindPivotIndex {
+package org.algorithmsAndDataStructures.prefixSum;
+// https://leetcode.com/problems/find-pivot-index/?envType=study-plan-v2&envId=leetcode-75
+
+public class FindPivotIndex
+{
+    public static int pivotIndex(int[] nums)
+    {
+        if (nums.length == 1)
+        {
+            return 0;
+        }
+
+        var nums1 = new int[nums.length];
+        var nums2 = new int[nums.length];
+
+        nums1[0] = nums[0];
+        nums2[0] = nums[nums.length - 1];
+
+        for (int i = 1; i < nums.length; i++)
+        {
+            nums1[i] = nums[i] + nums1[i - 1];
+            nums2[i] = nums[nums.length - i - 1] + nums2[i - 1];
+        }
+
+        int l;
+        int r;
+//        int preR;
+//        int preL;
+//        int nextR;
+//        int nextL;
+        int ans = -1;
+
+        for (int i = 0; i < nums.length; i++)
+        {
+            l = nums1[i];
+            r = nums2[nums2.length - 1 - i];
+
+            if (r == l)
+            {
+                ans = i;
+                break;
+            }
+
+//            if (i == 0)
+//            {
+//                preL = 0;
+//                nextL = nums1[i + 1];
+//                preR = 0;
+//                nextR = nums2[nums2.length - i - 2];
+//            }
+//            else if (i == nums.length - 1)
+//            {
+//                nextL = 0;
+//                preL = nums1[i - 1];
+//                nextR = 0;
+//                preR = nums2[nums2.length - i];
+//            }
+//            else
+//            {
+//                preL = nums1[i - 1];
+//                nextL = nums1[i + 1];
+//                preR = nums2[nums2.length - i];
+//                nextR = nums2[nums2.length - i - 2];
+//            }
+//
+//            l = nums1[i];
+//            r = nums2[nums2.length - 1 - i];
+//
+//            if (r == l && (preL == nextR || preR == nextL))
+//            {
+//                ans = i;
+//                break;
+//            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(pivotIndex(new int[] {2,1,-1}));
+    }
 }
