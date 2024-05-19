accf58cb82623a982accc6d737dc06f0c917a74e
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-05-19 03:17:02 +0300

    feat: tasks

---
+++ src/main/java/org/algorithmsAndDataStructures/HeapPriorityQueue/SmallestNumberInInfiniteSet.java
@@ -0,0 +1,10 @@
+package org.algorithmsAndDataStructures.HeapPriorityQueue;
+// https://leetcode.com/problems/smallest-number-in-infinite-set/description/?envType=study-plan-v2&envId=leetcode-75
+
+public class SmallestNumberInInfiniteSet
+{
+    public static void main(String[] args)
+    {
+        var tmp = new SmallestInfiniteSet();
+    }
+}
